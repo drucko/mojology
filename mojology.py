e2f4b64dcabe4ea34e1f2f7c449acbf32bc318db
Gergely Nagy <algernon@madhouse-project.org>
2011-01-07 02:26:35 +0100

    Added a @templated() decorator

--- mojology.py
+++ mojology.py
@@ -3,6 +3,7 @@ from __future__ import with_statement
 
 from flask import Flask, render_template, g, abort
 import pymongo, pymongo.objectid
+from mojology.utils import templated
 
 app = Flask (__name__)
 
@@ -26,20 +27,23 @@ def connect_mongo ():
     g.coll = g.mongo[config['mongodb']['db']][config['mongodb']['coll']]
 
 @app.route ("/")
+@templated ()
 def dashboard ():
     logs = g.coll.find (sort = [('date', -1)], limit = 15)
-    return render_template ("index.html", logs = logs)
+    return dict (logs = logs)
 
 @app.route("/host/<hostname>")
+@templated ()
 def host(hostname):
     logs = g.coll.find (spec = {'host': hostname},
                         sort = [('date', -1)], limit = 15)
     if logs.count () == 0:
         abort (404)
-    return render_template ("host.html", logs = logs,
-                            hostname = hostname)
+    return dict (logs = logs,
+                 hostname = hostname)
 
 @app.route("/log/<logid>")
+@templated ()
 def log (logid):
     try:
         oid = pymongo.objectid.ObjectId (logid)
@@ -51,7 +55,7 @@ def log (logid):
     if not entry:
         abort (404)
 
-    return render_template ('log.html', log = entry)
+    return dict (log = entry)
 
 if __name__ == "__main__":
     app.secret_key = '996ac3c8-19e2-11e0-806d-00248c0e4414'
