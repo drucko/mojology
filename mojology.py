f318ed9bdecaae88bbb3eaad7a2c69e5e8120dfe
Gergely Nagy <algernon@balabit.hu>
2011-01-07 21:15:43 +0100

    COPYING and copyright notices.

--- mojology.py
+++ mojology.py
@@ -1,4 +1,21 @@
 #! /usr/bin/env python
+##
+## mojology - a syslog browser with style
+## Copyright (C) 2011  Gergely Nagy <algernon@balabit.hu>
+##
+## Mojology is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Mojology is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import with_statement
 
 from flask import Flask, g, abort, url_for
