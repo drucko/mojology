bf1ae1da98350f775a55a7a2e8b5f614aff3fd61
Gergely Nagy <algernon@balabit.hu>
2011-01-08 16:21:42 +0100

    Table reorganisation for Firefox.
    For some reason, Firefox had issues rendering the table layout
    before. Shuffled a few things around, and now it displays the same in
    both Firefox and Chrome.

--- static/js/mojology.js
+++ static/js/mojology.js
@@ -1,19 +1,19 @@
 $(document).ready (function () {
 		       function log_details_set (self, data) {
-			   $("#log_details").remove ();
-			   $(self).after ("<tr id=\"log_details\" style=\"display: none;\"><td colspan=\"5\">" + data + "</td></tr>");
-			   $("#log_details").fadeIn (500);
+			   $(".log_details").remove ();
+			   $(self).after (data);
+			   $(".log_details").css ({display: "none"}).fadeIn (500);
 		       }
 		       function log_details_change (self, data) {
-			   if ($("#log_details").attr ('id')) {
-			       $("#log_details").fadeOut (500, function () { log_details_set (self, data); });
+			   if ($(".log_details").attr ('class')) {
+			       $(".log_details").fadeOut (500, function () { log_details_set (self, data); });
 			   } else {
 			       log_details_set (self, data);
 			   }
 		       }
 		       function log_details_fiddle (obj) {
-			   if (obj.next ().attr ('id') == "log_details") {
-			       $("#log_details").fadeToggle ("fast");
+			   if (obj.next ().attr ('class') == "log_details") {
+			       $(".log_details").fadeToggle ("fast");
 			       return;
 			   }
 			   $.get ("/log/" + obj.attr ('data-id') + "/dyn",
