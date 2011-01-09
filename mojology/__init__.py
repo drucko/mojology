6d5831e39e2fbf13bc300de5a728d20b56608a2c
Gergely Nagy <algernon@balabit.hu>
2011-01-09 22:17:39 +0100

    Added "logs" to the main menu.
    Explicitly added "logs" to the main menu, now that we have a couple of
    entries, and clicking on the logo wasn't obvious enough anymore.
    
    As a side effect, clicking on the logo brings one to the main
    dashboard (which, at the moment, is just an internal redirect to the
    browser dashboard).

--- mojology/__init__.py
+++ mojology/__init__.py
@@ -14,7 +14,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from flask import Flask, g, current_app
+from flask import Flask, g, current_app, redirect
 import pymongo
 import datetime
 
@@ -56,5 +56,9 @@ def Mojology (config_file = None, config_object = None):
     @templated ()
     def about ():
         return None
+
+    @app.route ("/")
+    def dashboard ():
+        redirect (url_for ("browser.dashboard"))
 
     return app
